0d3d9ea031036cd0c1ebe2d60b2b99155b0febb4
Eric Huang <eric456662003@gmail.com>
2025-03-04 17:24:52 +0800

    hw1 CRUDController done.

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,8 +4,13 @@ import com.example.demo.entity.Users;
 import com.example.demo.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver;
 
 @RestController
 @RequestMapping("/users")
@@ -14,7 +19,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public String createUser(@RequestBody Users user) {
         userService.addUser(user);
@@ -24,5 +29,20 @@ public class UserController {
     @GetMapping
     public List<Users> getUsers() {
         return userService.getAllUser();
+    }
+
+    @PutMapping("/{id}")
+    public String updateUser(@PathVariable String id, @RequestBody Users user) {
+        // TODO: process PUT request
+        userService.updateUser(Long.valueOf(id), user);
+
+        return "Successfully Updated User!";
+
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable String id) {
+        userService.deleteUser(Long.valueOf(id));
+        return "Successfully Deleted User!";
     }
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,12 +15,38 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    public Optional<Users> findById(Long id) {
+        return Optional.ofNullable(userRepository.findAll().stream()
+                .filter(user -> user.getId() == id)
+                .findFirst()
+                .orElse(null));
+    }
+
     public List<Users> getAllUser() {
         return userRepository.findAll();
     }
 
     public void addUser(Users user) {
         userRepository.save(user);
+    }
+
+    public void updateUser(Long id, Users user) {
+        Optional<Users> existUser = userRepository.findById(id);
+        if (existUser.isPresent()) {
+            user.setId(id);
+            userRepository.save(user);
+        } else {
+            throw new RuntimeException("User id not found");
+        }
+    }
+
+    public void deleteUser(Long id) {
+        Optional<Users> existUser = userRepository.findById(id);
+        if (existUser.isPresent()) {
+            userRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("User id not found");
+        }
     }
 
 }
