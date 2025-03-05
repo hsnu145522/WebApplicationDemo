90fab7b6f4f849a0e5c94de4a106feb91eee8d07
Eric Huang <eric456662003@gmail.com>
2025-03-05 11:53:37 +0800

    Log Added to DemoApplication

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,12 +5,9 @@ import com.example.demo.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver;
 
 @RestController
 @RequestMapping("/users")
