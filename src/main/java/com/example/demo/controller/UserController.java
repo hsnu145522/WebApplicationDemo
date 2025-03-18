4ca9081f8905b4db6679fef4b703f7fd9030a480
Eric Huang <eric456662003@gmail.com>
2025-03-18 17:18:49 +0800

    Week5 Swagger practice done

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,11 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.entity.Users;
 import com.example.demo.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/users")
@@ -18,6 +20,9 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
+    @Operation(summary = "Create new user", responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully created user")
+    })
     public Users createUser(@RequestBody Users user) {
         userService.addUser(user);
         return user;
@@ -25,11 +30,17 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all users", responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved all users")
+    })
     public List<Users> getUsers() {
         return userService.getAllUser();
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update user", responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated user")
+    })
     public Users updateUser(@PathVariable String id, @RequestBody Users user) {
         // TODO: process PUT request
         userService.updateUser(Long.valueOf(id), user);
@@ -40,6 +51,9 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete user", responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted user")
+    })
     public String deleteUser(@PathVariable String id) {
         userService.deleteUser(Long.valueOf(id));
         return "Successfully Deleted User!";
